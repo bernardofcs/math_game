c28b78f8cbf2f7d2aa51d6ccdebf98a76c777a0a
Bernardo Siqueira <bernardofcs@hotmail.com>
2017-03-13 19:04:02 +0000

    initial commit

---
+++ player.rb
@@ -0,0 +1,10 @@
+class Player
+  def initialize
+    @lives = 3
+  end
+
+  def lose_life
+    @lives--
+  end
+  
+end
---
+++ turn.rb
@@ -0,0 +1,6 @@
+class Turn
+  @@turns_left
+  def initialize
+    @@turns_left-- 
+  end
+end
